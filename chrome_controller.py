897f9f3c3e87ff27419e018f33888b0c2e64a4ea
Alex Yancey <me@alexyancey.com>
2024-02-01 03:18:57 -0800

    Run an idle animation if a button hasn't been pressed for more than 10 seconds

--- chrome_controller.py
+++ chrome_controller.py
@@ -1,16 +1,15 @@
+import threading
 import time
 from selenium import webdriver
 from pyjoystick.sdl2 import Key, Joystick, run_event_loop
 from selenium.webdriver.chrome.options import Options
 
-
 options = Options()
 options.add_argument("--window-size=1000,1000")
 options.add_experimental_option("useAutomationExtension", False)
-options.add_experimental_option("excludeSwitches",["enable-automation"])
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
 driver = webdriver.Chrome(options=options)
-
 
 driver.get("http://localhost:63342/WebGL-Fluid-Simulation/index.html")
 print(driver.title)
@@ -48,22 +47,38 @@ buttonKeyMap = {
     3: "H4"
 }
 
+last_pressed = time.time()
+
 
 def key_received(key):
+    global last_pressed
     if key.keytype == "Axis":
         return
 
     if key.number in buttonKeyMap:
         hex_pressed = buttonKeyMap[key.number]
         if key.value:
-            ret_data = driver.execute_script(f"return startButtonIterAnimation(\"{hex_pressed}\", {buttonPositionMap[hex_pressed]['x']}, {buttonPositionMap[hex_pressed]['y']}, {buttonPositionMap[hex_pressed]['x_delta']}, {buttonPositionMap[hex_pressed]['y_delta']});")
-
+            ret_data = driver.execute_script(
+                f"return startButtonIterAnimation(\"{hex_pressed}\", {buttonPositionMap[hex_pressed]['x']}, {buttonPositionMap[hex_pressed]['y']}, {buttonPositionMap[hex_pressed]['x_delta']}, {buttonPositionMap[hex_pressed]['y_delta']});")
+            last_pressed = time.time()
             buttonPositionMap[hex_pressed]["handle"] = ret_data["handle"]
             buttonPositionMap[hex_pressed]["pointer_id"] = ret_data["pointer_id"]
         else:
             if buttonPositionMap[hex_pressed]["handle"]:
                 driver.execute_script(f"clearInterval({buttonPositionMap[hex_pressed]['handle']});")
-                driver.execute_script(f"pointers.splice(pointers.findIndex(v => v.id === {buttonPositionMap[hex_pressed]['pointer_id']}), 1);")
+                driver.execute_script(
+                    f"pointers.splice(pointers.findIndex(v => v.id === {buttonPositionMap[hex_pressed]['pointer_id']}), 1);")
 
+
+def check_last_pressed():
+    global last_pressed
+    while True:
+        if time.time() - last_pressed > 10:
+            driver.execute_script(f"splatStack.push(parseInt(Math.random() * 20) + 5);")
+
+        time.sleep(1)
+
+
+threading.Thread(target=check_last_pressed).start()
 
 run_event_loop(print_add, print_remove, key_received)
